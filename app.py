5e8c46638b7e8304c0dea69731410b8ec0215812
4MIR2000 <taghizadegan.amir@yahoo.de>
2026-05-20 09:21:17 +0200

    render file

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ import requests
 import streamlit as st
 
 API_URL = "http://localhost:8000"
+BACKEND_URL = os.getenv("BACKEND_URL", "http://localhost:8000")
 
 load_dotenv()  # Lädt die .env Datei
 
